6ea9c3b9cd82b7183d519ca7508d6f6fd99cea05
Ali Sanaknaki <asana085@uottawa.ca>
2018-12-09 11:49:48 -0500

    init

---
+++ js/tac.js
@@ -0,0 +1,68 @@
+// Variables
+var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+var YOU = 1;
+var BOT = -1;
+var who = 1;
+
+function selectCell(cell) {
+
+	// You decided to go first
+	var botStartButton = document.getElementById("btnStart");
+	botStartButton.disabled = true;
+
+	var x = cell.id.split("")[0];
+	var y = cell.id.split("")[1];
+	if(who == 1) {
+		var move = placeMove(x, y, YOU);
+		if (move == true) {
+			cell.innerHTML = "X";
+			checkWin();
+			who = 0;
+		}
+	} else if(who == 0) {
+		var move = placeMove(x, y, BOT);
+		if (move == true) {
+			cell.innerHTML = "O";
+			who = 1;
+		}
+	} 
+}
+
+function placeMove(x, y, val) {
+	if (moveIsValid(x, y)) {
+		board[x][y] = val;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function moveIsValid(x, y) {
+	if(board[x][y] == 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function checkWin() {
+	console.log("CALLED")
+	console.log(board);
+	if((board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1) ||
+	   (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) ||
+	   (board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1) ||
+	   (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) ||
+	   (board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1) ||
+	   (board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1) ||
+	   (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) || 
+	   (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1)) {
+			var mess = document.getElementById("message");
+			mess.innerHTML = "YOU WIN!";
+			console.log("WOO")	
+	}
+	// if (board === [[1, 0, 0], [1, 0, 0], [1, 0, 0]] || board == [[0, 1, 0], [0, 1, 0], [0, 1, 0]] || board == [[0, 0, 1], [0, 0, 1], [0, 0, 1]] || board == [[1, 1, 1], [0, 0, 0], [0, 0, 0]] || board == [[0, 0, 0], [1, 1, 1], [0, 0, 0]] || board == [[0, 0, 0], [0, 0, 0], [1, 1, 1]] || board == [[1, 0, 0], [0, 1, 0], [0, 0, 1]] || board == [[0, 0, 1], [0, 1, 0], [1, 0, 0]]) {
+	// 	var mess = document.getElementById("message");
+	// 	mess.innerHTML = "YOU WIN!";
+	// 	console.log("WOO")
+	// }
+}
