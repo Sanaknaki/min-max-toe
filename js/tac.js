f498fb9d79cdc715c211c690c26d5185f264724e
Ali Sanaknaki <asana085@uottawa.ca>
2018-12-09 13:06:08 -0500

    Cleaner code?

--- js/tac.js
+++ js/tac.js
@@ -4,6 +4,27 @@ var YOU = 1;
 var BOT = -1;
 var who = 1;
 
+/* 
+ * Cells that are left on the board to be chosen
+*/
+function cellsAvailable(current_state){
+	var cellsAvailable = [];
+
+	// Go over the board 'current_state' and find all open cells.
+	for (var x = 0; x < 3; x++) {
+		for (var y = 0; y < 3; y++) {
+			if (current_state[x][y] == 0)
+				cells.push([x, y]);
+		}
+	}
+
+	// Return list of cells.
+	return cellsAvailable;
+}
+
+/* 
+ * Select a cell on the board and place move.
+*/
 function selectCell(cell) {
 
 	// You decided to go first
@@ -12,23 +33,34 @@ function selectCell(cell) {
 
 	var x = cell.id.split("")[0];
 	var y = cell.id.split("")[1];
+	
 	if(who == 1) {
 		var move = placeMove(x, y, YOU);
 		if (move == true) {
 			cell.innerHTML = "X";
-			checkWin();
 			who = 0;
 		}
 	} else if(who == 0) {
 		var move = placeMove(x, y, BOT);
 		if (move == true) {
 			cell.innerHTML = "O";
-			checkWin();
 			who = 1;
 		}
-	} 
+	}
+
+	if (gameIsOver(board) > 0) {
+		var mess = document.getElementById("message");
+		mess.innerHTML = "YOU WIN!";
+	} else if (gameIsOver(board) < 0) {
+		var mess = document.getElementById("message");
+		mess.innerHTML = "YOU LOSE!";	
+	}
 }
 
+/* 
+ * Place a move at position (x,y).
+ * Assign player value to board.
+*/
 function placeMove(x, y, val) {
 	if (moveIsValid(x, y)) {
 		board[x][y] = val;
@@ -38,6 +70,9 @@ function placeMove(x, y, val) {
 	}
 }
 
+/* 
+ * Check if a movie is value before placing at position (x,y).
+*/
 function moveIsValid(x, y) {
 	if(board[x][y] == 0) {
 		return true;
@@ -46,27 +81,78 @@ function moveIsValid(x, y) {
 	}
 }
 
-function checkWin() {
-	console.log(board);
-	if((board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1) ||
-	   (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) ||
-	   (board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1) ||
-	   (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) ||
-	   (board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1) ||
-	   (board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1) ||
-	   (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) || 
-	   (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1)) {
-			var mess = document.getElementById("message");
-			mess.innerHTML = "X WINS!";
-	} else if ((board[0][0] === -1 && board[1][0] === -1 && board[2][0] === -1) ||
-	   (board[0][1] === -1 && board[1][1] === -1 && board[2][1] === -1) ||
-	   (board[0][2] === -1 && board[1][2] === -1 && board[2][2] === -1) ||
-	   (board[0][0] === -1 && board[0][1] === -1 && board[0][2] === -1) ||
-	   (board[1][0] === -1 && board[1][1] === -1 && board[1][2] === -1) ||
-	   (board[2][0] === -1 && board[2][1] === -1 && board[2][2] === -1) ||
-	   (board[0][0] === -1 && board[1][1] === -1 && board[2][2] === -1) || 
-	   (board[0][2] === -1 && board[1][1] === -1 && board[2][0] === -1)) {
-		var mess = document.getElementById("message");
-		mess.innerHTML = "O WINS!";
+// function checkWin() {
+// 	console.log(board);
+// 	if((board[0][0] === 1 && board[1][0] === 1 && board[2][0] === 1) ||
+// 	   (board[0][1] === 1 && board[1][1] === 1 && board[2][1] === 1) ||
+// 	   (board[0][2] === 1 && board[1][2] === 1 && board[2][2] === 1) ||
+// 	   (board[0][0] === 1 && board[0][1] === 1 && board[0][2] === 1) ||
+// 	   (board[1][0] === 1 && board[1][1] === 1 && board[1][2] === 1) ||
+// 	   (board[2][0] === 1 && board[2][1] === 1 && board[2][2] === 1) ||
+// 	   (board[0][0] === 1 && board[1][1] === 1 && board[2][2] === 1) || 
+// 	   (board[0][2] === 1 && board[1][1] === 1 && board[2][0] === 1)) {
+// 			var mess = document.getElementById("message");
+// 			mess.innerHTML = "X WINS!";
+// 	} else if ((board[0][0] === -1 && board[1][0] === -1 && board[2][0] === -1) ||
+// 	   (board[0][1] === -1 && board[1][1] === -1 && board[2][1] === -1) ||
+// 	   (board[0][2] === -1 && board[1][2] === -1 && board[2][2] === -1) ||
+// 	   (board[0][0] === -1 && board[0][1] === -1 && board[0][2] === -1) ||
+// 	   (board[1][0] === -1 && board[1][1] === -1 && board[1][2] === -1) ||
+// 	   (board[2][0] === -1 && board[2][1] === -1 && board[2][2] === -1) ||
+// 	   (board[0][0] === -1 && board[1][1] === -1 && board[2][2] === -1) || 
+// 	   (board[0][2] === -1 && board[1][1] === -1 && board[2][0] === -1)) {
+// 		var mess = document.getElementById("message");
+// 		mess.innerHTML = "O WINS!";
+// 	}
+// }
+
+/* 
+ * Given a game state and a player number, check if either of them win.
+*/
+function checkGameOver(state, playerValue) {
+	// All states where someone wins
+	var allStates = [[state[0][0], state[0][1], state[0][2]],
+					 [state[1][0], state[1][1], state[1][2]],
+					 [state[2][0], state[2][1], state[2][2]],
+					 [state[0][0], state[1][0], state[2][0]],
+					 [state[0][1], state[1][1], state[2][1]],
+					 [state[0][2], state[1][2], state[2][2]],
+					 [state[0][0], state[1][1], state[2][2]],
+					 [state[2][0], state[1][1], state[0][2]]];
+
+	// For all possible states, check if you won.
+	for (var i = 0; i < 8; i++) {
+		var singleWinState = allStates[i];
+		var hasPlayer = 0;
+
+		// Check a win state if you won.
+		for (var j = 0; j < 3; j++) {
+			if (singleWinState[j] == playerValue)
+				hasPlayer++;
+		}
+
+		// 3 in a row = win!
+		if (hasPlayer == 3)
+			return true;
+	}
+
+	return false;
+}
+
+/* 
+ * Send same state, with both player and bot value.
+ * Return the winner.
+*/
+function gameIsOver(state) {
+	if(checkGameOver(state, YOU) == true) {
+		console.log("You win!")
+		return 1;
+	} else {
+		if(checkGameOver(state, BOT) == true) {
+			console.log("You win!")
+			return -1;
+		}
+
+		return 0;
 	}
 }
